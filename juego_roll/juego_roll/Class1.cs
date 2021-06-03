94cd9c04cf8542cc0215ee70b0e8289757d1d34b
Leonardo Contreras <leoocontreras02@gmail.com>
2021-06-03 17:58:03 -0300

    commit 2

--- juego_roll/juego_roll/Class1.cs
+++ juego_roll/juego_roll/Class1.cs
@@ -4,40 +4,41 @@ using System.Text;
 
 namespace juego_roll
 {
-    class Datos
+    enum tipoPersonaje
     {
-        private string tipo;
+        humano,
+        orco,
+        elfo,
+        enano,
+        hobbit
+    }
+    public class Personaje
+    {
+        private tipoPersonaje tipo;
         private string nombre;
         private string apodo;
         private DateTime fechaNacimiento;
         private int edad;
         private int salud;
 
-        public string Tipo { get => tipo; set => tipo = value; }
-        public string Nombre { get => nombre; set => nombre = value; }
-        public string Apodo { get => apodo; set => apodo = value; }
-        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
-        public int Edad { get => edad; set => edad = value; }
-        public int Salud { get => salud; set => salud = value; }
-    }
-
-    class Caracteristicas
-    {
         private int velocidad;
         private int destresa;
         private int fuerza;
         private int nivel;
         private int armadura;
 
+
+        
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Salud { get => salud; set => salud = value; }
         public int Velocidad { get => velocidad; set => velocidad = value; }
         public int Destresa { get => destresa; set => destresa = value; }
         public int Fuerza { get => fuerza; set => fuerza = value; }
         public int Nivel { get => nivel; set => nivel = value; }
         public int Armadura { get => armadura; set => armadura = value; }
-
-        public void caracteristicasPersonaje()
-        {
-            
-        }
+        internal tipoPersonaje Tipo { get => tipo; set => tipo = value; }
     }
 }
--- juego_roll/juego_roll/Program.cs
+++ juego_roll/juego_roll/Program.cs
@@ -6,7 +6,17 @@ namespace juego_roll
     {
         static void Main(string[] args)
         {
-            
+            Personaje personaje = new Personaje();
+
+            personaje.Nombre = Console.ReadLine();
+            personaje.Apodo = Console.ReadLine();
+            string fecha = Console.ReadLine();
+            personaje.FechaNacimiento = Convert.ToDateTime(fecha);
+            personaje.Salud = Convert.ToInt32(Console.ReadLine());
+        }
+        public void cargaDatos()
+        {
+
         }
     }
 }
